dab4b3755bef2fb077520da699d42c38f3eb1ff7
Priyanshi <kalkhandaypriyanshi@gmail.com>
2026-08-18 14:26:01 +0530

    Add files via upload

---
+++ labsheet1_priyanshi_cu25250081.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {10, 20, 30, 40, 50};
+
+    cout << "Vector before pop_back(): ";
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    arr.pop_back();
+
+    cout << "\nVector after pop_back(): ";
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr;
+    int n, element;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> element;
+        arr.push_back(element);
+    }
+
+    cout << "\nElements in the vector are: ";
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
